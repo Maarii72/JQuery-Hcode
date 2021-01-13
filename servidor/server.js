5319fee9478ae6d9932182a66205510077672c5c
Marilena Sales <marilena.sales@gmail.com>
2021-01-13 16:29:03 -0300

    1

--- servidor/server.js
+++ servidor/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const nedb =require('nedb')
+const bodyParser = require('body-parser')
+const db = nedb({filena,e: './database.db', autoload: true})
 
 const app = express()
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/books', (req, res) =>{
     res.json({
         success:true
     })
+})
+
+app.post('/books', (req, res) => {
+    res.json(req,body)
 })
 
 app.listen(3000, () =>{
